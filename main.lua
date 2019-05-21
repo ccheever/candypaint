a1eab275b83fa3a0828e34d286cfafc52ee8c64f
Charlie Cheever <ccheever@expo.io>
2019-05-21 01:31:18 -0700

    Adding color picker

--- main.lua
+++ main.lua
@@ -7,13 +7,19 @@ local WIDTH = 800
 local TOP = 0
 local LEFT = 0
 
+local color = {0.0, 0.0, 0.0, 1.0}
+
+local cui = castle.ui
+
+local log =
+  require("https://raw.githubusercontent.com/ccheever/castle-utils/c5a150bf783bfcaf24bbcf8cbe0824fae34a8198/log.lua")
+
 function love.load()
   -- love.mouse.setVisible(true)
   love.mouse.setVisible(false) -- make default mouse invisible
   img = love.graphics.newImage("cross-black.png") -- load in a custom mouse image
   canvas = love.graphics.newCanvas(WIDTH, HEIGHT)
   lastX, lastY = love.mouse.getPosition()
-
 end
 
 function love.draw()
@@ -24,14 +30,13 @@ function love.draw()
   -- print("x,y = ",x,y)
   love.graphics.draw(img, x, y) -- draw the custom mouse image
   love.graphics.draw(canvas, LEFT, TOP)
-
 end
 
 function love.update()
   local x, y = love.mouse.getPosition()
   if love.mouse.isDown(1) then
     love.graphics.setCanvas(canvas)
-    love.graphics.setColor(1.0, 0.0, 0.0, 1.0)
+    love.graphics.setColor(color[1], color[2], color[3], color[4])
     love.graphics.line(lastX, lastY, x, y)
     love.graphics.setCanvas()
   end
@@ -44,5 +49,36 @@ function love.keypressed(key, scancode, isrepeat)
     love.graphics.setColor(1.0, 1.0, 1.0, 1.0)
     love.graphics.rectangle("fill", 0, 0, WIDTH, HEIGHT)
     love.graphics.setCanvas()
+  end
+end
+
+function castle.uiupdate()
+  castle.ui.text("Hello, world!")
+  if cui.button("red") then
+    color = {1.0, 0.0, 0.0, 1.0}
+  end
+  if cui.button("green") then
+    color = {0.0, 1.0, 0.0, 1.0}
+  end
+  if cui.button("blue") then
+    color = {0.0, 0.0, 1.0, 1.0}
+  end
+  if cui.button("gold") then
+    color = {1.0, 0.8, 0.0, 1.0}
+  end
+  if cui.button("light gray") then
+    color = {0.8, 0.8, 0.8, 1.0}
+  end
+  if cui.button("dark gray") then
+    color = {0.3, 0.3, 0.3, 1.0}
+  end
+  if cui.button("purple") then
+    color = {0.7, 0.0, 0.7, 1.0}
+  end
+  if cui.button("black") then
+    color = {0.0, 0.0, 0.0, 1.0}
+  end
+  if cui.button("white") then
+    color = {1.0, 1.0, 1.0, 1.0}
   end
 end
